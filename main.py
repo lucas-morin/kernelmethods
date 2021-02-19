aca5060e392707224e636ff339404b186e18c21e
lucas <lucas.morin@grenoble-inp.org>
2021-02-19 16:08:47 +0100

    final commit

--- main.py
+++ main.py
@@ -10,52 +10,53 @@ from tools import *
 from models import *
 from kernels import *
 
+#Yte stores submission predictions
 Yte = []
 
-#Define model hyperparameters best suited for each dataset (To modify regarding the model used)
-
-#lambda_ = [100, 100, 100] #SVR
-lambda_ = [1e-10, 1e-10, 1e-10] #KLR
-
-#k = [9, 9, 8] #Mismatch Kernel 
-#nb_mismatch = 0 #Mismatch Kernel 
+#Define model hyperparameters best suited for each dataset (To comment/uncomment)
+lambda_ = [100, 100, 100] #SVR
+#lambda_ = [1e-10, 1e-10, 1e-10] #KLR
+k = [9, 9, 8] #Mismatch Kernel 
+nb_mismatch = 2 #Mismatch Kernel 
 #degree = [4, 4, 4] #Polynomial Kernel 
 #gamma = [None, None, None] #Gaussian Kernel 
 
 #Iterate over the 3 data sets
 for i in range(3):
 
-    #Read training data
-    #x_train = pd.read_csv(f"./data/Xtr{i}.csv", index_col=0)
-    #y_train = pd.read_csv(f"./data/Ytr{i}.csv", index_col=0)
-    x_train = read_embedded_data(f"./data/Xtr{i}_mat100.csv") 
-    y_train = pd.read_csv(f"./data/Ytr{i}.csv", index_col=0)
+    #Read training data (To comment/uncomment)
+    x_train = pd.read_csv(f"./data/Xtr{i}.csv", index_col=0) #Raw sequences
+    y_train = pd.read_csv(f"./data/Ytr{i}.csv", index_col=0) #Raw sequences
+    #x_train = read_embedded_data(f"./data/Xtr{i}_mat100.csv") #Numerical data
+    #y_train = pd.read_csv(f"./data/Ytr{i}.csv", index_col=0) #Numerical data
 
-    #Augment training data (optional)
-    #x_train, y_train = augment(x_train, y_train)
+    #Augment training data (Only for raw sequences)
+    x_train, y_train = augment(x_train, y_train)
     
-    #Read test input
-    #x_test = pd.read_csv(f"./data/Xte{i}.csv", index_col=0)
-    x_test = read_embedded_data(f"./data/Xte{i}_mat100.csv") 
+    #Read test input (To comment/uncomment)
+    x_test = pd.read_csv(f"./data/Xte{i}.csv", index_col=0) #Raw sequences
+    #x_test = read_embedded_data(f"./data/Xte{i}_mat100.csv") #Numerical data
 
-    #Create kernel
-    #kernel = MismatchKernel(k = k[i])
+    #Create kernel (To comment/uncomment)
+    kernel = MismatchKernel(k = k[i], nb_mismatch = nb_mismatch)
     #kernel = GaussianKernel(gamma = gamma[i])
-    kernel = PolynomialKernel(degree = degree[i])
+    #kernel = PolynomialKernel(degree = degree[i])
 
-    #Create model
-    #model = SVR(lambda_ = lambda_[i], kernel = kernel) 
-    model = KLR(lambda_=lambda_[i], kernel=kernel) 
+    #Create model (To comment/uncomment)
+    model = SVR(lambda_ = lambda_[i], kernel = kernel) 
+    #model = KRR(lambda_=lambda_[i], kernel=kernel) 
 
-    #Change labels from 0, 1 to -1, 1 
-    #y_train = y_train.applymap(lambda x: 1 if (x == 1) else -1)
+    #Change labels from 0, 1 to -1, 1 (Only for SVM)
+    y_train = y_train.applymap(lambda x: 1 if (x == 1) else -1) 
 
     #Train the model
     model.fit(x_train, y_train)
 
-    #Predict new labels
-    Yte = np.concatenate((Yte, (model.predict(x_test) > 0).astype(int)))
-    
+    #Predict new labels (To comment/uncomment)
+    #Yte = np.concatenate((Yte, (model.predict(x_test) > 1/2).astype(int))) #KRR
+    Yte = np.concatenate((Yte, (model.predict(x_test) > 0).astype(int))) #SVM
+
+#Submission is stored in ./data/Yte.csv 
 Yte = pd.DataFrame(Yte, index=range(3000)).astype(int)
 Yte.to_csv(path_or_buf="./data/Yte.csv", header=['Bound'], index_label="Id", index=True)
 
--- tuning.py
+++ tuning.py
@@ -11,49 +11,51 @@ from tools import *
 from models import *
 from kernels import *
 
-#Define model hyperparameters best suited (To modify regarding the model used)
+#Define model hyperparameters best suited (To comment/uncomment)
 lambda_ = 100 #SVR
 #lambda_ = 1e-10 #KLR
 k = 9 #Mismatch Kernel 
-nb_mismatch = 2 #Mismatch Kernel 
+nb_mismatch = 1 #Mismatch Kernel 
 #degree = 7 #Polynomial Kernel 
 #gamma = None #Gaussian Kernel 
 
 #Choose a dataset
-i = 1
+i = 2
 
-#Read anotated data
-x_train = pd.read_csv(f"./data/Xtr{i}.csv", index_col=0)
-y_train = pd.read_csv(f"./data/Ytr{i}.csv", index_col=0)
-#x_train = read_embedded_data(f"./data/Xtr{i}_mat100.csv") 
-#y_train = pd.read_csv(f"./data/Ytr{i}.csv", index_col=0)
+#Read anotated data (To comment/uncomment)
+x_train = pd.read_csv(f"./data/Xtr{i}.csv", index_col=0) #Raw sequences
+y_train = pd.read_csv(f"./data/Ytr{i}.csv", index_col=0) #Raw sequences
+#x_train = read_embedded_data(f"./data/Xtr{i}_mat100.csv") #Numerical data
+#y_train = pd.read_csv(f"./data/Ytr{i}.csv", index_col=0) #Numerical data
 
-#Augment anotated data (optional)
-#x_train, y_train = augment(x_train, y_train)
+#Augment anotated data (Only for raw sequences)
+x_train, y_train = augment(x_train, y_train)
 
 #Split anotated data in training and validation sets
 x_train, x_validation, y_train, y_validation = train_test_split(x_train, y_train, train_size=0.8, test_size=0.2)
 
-#Create kernel
+#Create kernel (To comment/uncomment)
 kernel = MismatchKernel(k = k, nb_mismatch = nb_mismatch)
 #kernel = GaussianKernel(gamma = gamma)
 #kernel = PolynomialKernel(degree = degree)
 
-#Create model
+#Create model (To comment/uncomment)
 model = SVR(lambda_ = lambda_, kernel = kernel) 
-#model = KLR(lambda_=lambda_, kernel=kernel) 
+#model = KRR(lambda_=lambda_, kernel=kernel) 
 
-#Change labels from 0, 1 to -1, 1 
+#Change labels from 0, 1 to -1, 1 (Only for SVM)
 y_train = y_train.applymap(lambda x: 1 if (x == 1) else -1)
 
 #Train the model
 model.fit(x_train, y_train)
 
-#Predict new labels
-predictions_train = (model.predict(x_train) > 0).astype(int)  
-predictions_validation = (model.predict(x_validation) > 0).astype(int)  
+#Predict new labels (To comment/uncomment)
+predictions_train = (model.predict(x_train) > 0).astype(int) #SVM
+predictions_validation = (model.predict(x_validation) > 0).astype(int) #SVM
+#predictions_train = (model.predict(x_train) > 1/2).astype(int) #KRR
+#predictions_validation = (model.predict(x_validation) > 1/2).astype(int) #KRR
 
-#Come back to 0, 1 labels
+#Come back to 0, 1 labels 
 y_train = y_train.applymap(lambda x: 1 if (x == 1) else 0)
 
 #Measure performances
